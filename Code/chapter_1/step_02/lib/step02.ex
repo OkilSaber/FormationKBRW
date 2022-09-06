b2161d1238572c69cb4c0a5d35704283cb08a9a3
OkilSaber <okil.lakhdari@kbrwadventure.com>
2022-09-06 11:24:53 +0200

    Final step2

--- Code/chapter_1/step_02/lib/step02.ex
+++ Code/chapter_1/step_02/lib/step02.ex
@@ -2,52 +2,43 @@ defmodule Step02 do
   use GenServer
 
   def start_link(opts) do
-    server = Keyword.fetch!(opts, :name)
-    GenServer.start_link(__MODULE__, server, opts)
+    table_name = Keyword.fetch!(opts, :name)
+
+    Tuple.insert_at(
+      GenServer.start_link(__MODULE__, []),
+      2,
+      :ets.new(table_name, [:named_table, read_concurrency: true])
+    )
   end
 
   @impl true
-  def init(table) do
-    IO.puts("Starting #{table} server")
-    names = :ets.new(table, [:named_table, read_concurrency: true])
-    refs = %{}
-    {:ok, {names, refs}}
+  def init(server) do
+    {:ok, server}
   end
 
-  def lookup(server, name) do
-    case :ets.lookup(server, name) do
-      [{^name, pid}] -> {:ok, pid}
-      [] -> :error
+  def cast(_server, {request, table, name}) do
+    case request do
+      :create ->
+        :ets.insert(table, {name, name})
+        {:noreply, name}
+
+      :delete ->
+        :ets.delete(table, name)
+        {:noreply, name}
     end
   end
 
-  def create(server, name) do
-    GenServer.cast(server, {:create, name})
-  end
-
-  @impl true
-  def handle_cast({:create, name}, {names, refs}) do
-    case lookup(names, name) do
-      {:ok, _pid} ->
-        {:noreply, {names, refs}}
-      :error ->
-        {:ok, pid} = DynamicSupervisor.start_child(MySupervisor, Bucket)
-        ref = Process.monitor(pid)
-        refs = Map.put(refs, ref, name)
-        :ets.insert(names, {name, pid})
-        {:noreply, {names, refs}}
+  def call(_server, {request, table, name}) do
+    case request do
+      :read ->
+        case :ets.lookup(table, name) do
+          [{_, value}] -> {:reply, value, name}
+          [] -> {:reply, nil, name}
+        end
     end
   end
 
-  @impl true
-  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
-    {name, refs} = Map.pop(refs, ref)
-    :ets.delete(names, name)
-    {:noreply, {names, refs}}
-  end
-
-  @impl true
-  def handle_info(_msg, state) do
-    {:noreply, state}
-  end
 end
+# {:ok, server, table} = Step02.start_link([name: :table])
+# Step02.cast(server, {:create, table, "test"})
+# Step02.call(server, {:read, table, "test"})
