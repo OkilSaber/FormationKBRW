98b78d789b055db99d06d51b9d44ad42cf7c718f
OkilSaber <okil.lakhdari@kbrwadventure.com>
2022-09-06 16:56:25 +0200

    Add step 4

---
+++ Code/chapter_1/step_04/lib/server/database.ex
@@ -0,0 +1,41 @@
+defmodule Server.Database do
+  use GenServer
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  @impl true
+  def init(state) do
+    :ets.new(__MODULE__, [:named_table, read_concurrency: true])
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_cast({request, key, value}, state) do
+    case request do
+      :create ->
+        :ets.insert(__MODULE__, {key, value})
+        {:noreply, state}
+
+      :update ->
+        :ets.insert(__MODULE__, {key, value})
+        {:noreply, state}
+    end
+  end
+
+  @impl true
+  def handle_cast({request, key}, state) do
+    case request do
+      :delete ->
+        :ets.delete(__MODULE__, key)
+        {:noreply, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:read, key}, _from, state) do
+    IO.puts("Reading #{key}")
+    {:reply, :ets.lookup(__MODULE__, key), state}
+  end
+end
---
+++ Code/chapter_1/step_04/test/step04_test.exs
@@ -0,0 +1,21 @@
+defmodule Step04Test do
+  use ExUnit.Case
+  doctest TutoKbrwStackEx
+
+  test "Create" do
+    assert GenServer.cast(Server.Database, {:create, "first", 0}) == :ok
+    assert GenServer.cast(Server.Database, {:create, "second", 1}) == :ok
+  end
+
+  test "Read" do
+    assert GenServer.call(Server.Database, {:read, "first"}) == [{"first", 0}]
+  end
+
+  test "Update" do
+    assert GenServer.cast(Server.Database, {:update, "first", 2}) == :ok
+  end
+
+  test "Delete" do
+    assert GenServer.cast(Server.Database, {:delete, "second"}) == :ok
+  end
+end
